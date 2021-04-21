1bb93f478a3c0527f17b254243eaccb40cb750e0
SadVitorGomez <7vitorgomez@gmail.com>
2021-04-21 12:45:29 -0300

    day 3

---
+++ src/utils/convertDurationToTimeString.ts
@@ -0,0 +1,11 @@
+export function convertDurationToTimeString(duration: number): string {
+  const hours = Math.floor(duration / 3600) // Seconds to hours
+  const minutes = Math.floor((duration % 3600) / 60)
+  const seconds = Math.floor(duration % 60)
+
+  const finalResult = [hours, minutes, seconds]
+    .map((unit) => String(unit).padStart(2, '0'))
+    .join(':')
+
+  return finalResult
+}
